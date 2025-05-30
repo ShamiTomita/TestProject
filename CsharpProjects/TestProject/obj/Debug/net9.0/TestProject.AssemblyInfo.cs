d426de89fa3dd214b682587839605eedbb0f2056
SHAMI_TOMITA <shamitomita@gmail.com>
2025-05-30 17:27:05 -0400

    using Math.max

--- CsharpProjects/TestProject/obj/Debug/net9.0/TestProject.AssemblyInfo.cs
+++ CsharpProjects/TestProject/obj/Debug/net9.0/TestProject.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("TestProject")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+73416135861785d9232bd89772968f363d78e6fe")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+d42b4d83f0f5d7374494970d23d40d9d788b9c1a")]
 [assembly: System.Reflection.AssemblyProductAttribute("TestProject")]
 [assembly: System.Reflection.AssemblyTitleAttribute("TestProject")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
